e87906bcfdfac7f0e265c369e3105aef01ff4e85
htsvr <htsvr@EB112-1.et.byu.edu>
2026-02-17 11:44:34 -0700

    Added Main file

---
+++ newmain.c
@@ -0,0 +1,25 @@
+/* 
+ * File:   newmain.c
+ * Author: htsvr
+ *
+ * Created on February 17, 2026, 11:43 AM
+ */
+#pragma config FNOSC = LPFRC
+//#pragma config ICS = PGx3
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "xc.h"
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    while(1) {
+        
+    }
+    
+    return (EXIT_SUCCESS);
+}
+
