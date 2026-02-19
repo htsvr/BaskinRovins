571dd6ae643489ea0c43d10d0de87e94468319eb
htsvr <brrgrra@gmail.com>
2026-02-19 15:32:03 -0700

    changed something

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -6,5 +6,5 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=BaskinRovins.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/BaskinRovins.X.production.hex
+CND_ARTIFACT_NAME_default=BaskinRovins.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/BaskinRovins.production.hex
