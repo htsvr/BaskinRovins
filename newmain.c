899c7a13da88d37515bfc3196681466ae0fb4938
htsvr <brrgrra@gmail.com>
2026-02-21 09:40:06 -0700

    Driving

--- newmain.c
+++ newmain.c
@@ -11,13 +11,36 @@
 #include <stdlib.h>
 #include "xc.h"
 
+enum State {
+    FORWARD,
+    TURN
+}
+
 /*
  * 
  */
 int main(int argc, char** argv) {
-    
+    static enum State state = FORWARD;
+    setTimer(1);
+
     while(1) {
-        
+        switch (state) {
+            case FORWARD:
+                if(timerFinished()) {
+                    state = TURN;
+                    setLeftMotor(FORWARD, 100);
+                    setRightMotor(FORWARD, 0);
+                    setLeftSteps(90);
+                }
+                break;
+            case TURN:
+           
+                break;
+            default:     if(angleReached()) {
+                    state = FORWARD;
+                }
+
+        }
     }
     
     return (EXIT_SUCCESS);
