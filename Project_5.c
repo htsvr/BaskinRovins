5a74bb22295de5a9ad5f71f09225a73c1a9cc7b6
Sophie Martin <sophiemartin@Sophies-MacBook-Air-229.local>
2026-02-21 09:20:46 -0700

    Milestone 5

---
+++ Project_5.c
@@ -0,0 +1,84 @@
+/*
+ * File:   Project_5.c
+ * Author: sophiemartin
+ *
+ * Created on February 21, 2026, 8:52 AM
+ */
+
+
+#include "xc.h"
+#pragma config FNOSC = FRCDIV
+
+// left motor: RA0 = DIR; RA1 = STEP
+// right motor: RB0 = DIR; RB1 = STEP
+
+
+int main(void) {
+    
+    ANSA = 0;
+    ANSB = 0;
+    LATA = 0;
+    LATB = 0;
+    
+    TRISAbits.TRISA0 = 0;
+    TRISAbits.TRISA1 = 0;
+    TRISBbits.TRISB0 = 0;
+    TRISBbits.TRISB1 = 0;
+    
+    long forwardDrive = 2000;
+    long turn90 = 400;
+    long turn180 = 800;
+    
+    void stepLeft() {
+        LATAbits.LATA1 = 1;
+        __delay_us(500);
+        LATAbits.LATA1 = 0;
+        __delay_us(500);
+    }
+
+    void stepRight() {
+        LATBbits.LATB1 = 1;
+        __delay_us(500);
+        LATBbits.LATB1 = 0;
+        __delay_us(500);
+    }
+    
+    void driveForward(long steps) {
+        LATAbits.LATA0 = 1;  // Left CW
+        LATBbits.LATB0 = 1;  // Right CW
+    
+        for(long i = 0; i < steps; i++) {
+            stepLeft();
+            stepRight();
+        }
+    }   
+    
+    void turnRight(long steps) {
+        LATAbits.LATA0 = 1;  // Left CW
+        LATBbits.LATB0 = 0;  // Right CCW
+    
+        for(long i = 0; i < steps; i++) {
+            stepLeft();
+            stepRight();
+        }
+    }
+    
+    while(1)
+    {
+        driveForward(forwardDrive);
+
+        turnRight(turn90);
+
+        driveForward(forwardDrive);
+
+        turnRight(turn180);
+
+        driveForward(forwardDrive);
+
+        while(1);
+    }
+    
+          
+    return 0;
+}
+    
